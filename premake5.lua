9ed4451993890fc29653256c40baa3d5261a1893
jonas <jonas.kristen01@gmail.com>
2020-11-27 16:28:22 +0100

    premake5 script to create a static lib

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "Spdlog"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    defines
+    {
+        "SPDLOG_COMPILED_LIB"
+    }
+
+    includedirs
+    {
+        "src",
+        "include"
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
